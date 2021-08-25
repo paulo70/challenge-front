297f4926e09e0f126bfed780941a07f851bb9d2f
Paulo Costa <paulofrontend@gmail.com>
2021-08-25 17:03:21 -0300

    Added page to choice your master

--- src/components/Masters/index.js
+++ src/components/Masters/index.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import Back from '../GoBack'
+
+import './master.scss'
+import DarthVader from '../../assets/images/darth-vader.png'
 
 const Master = () => {
     return (
-        <h1>Mestre</h1>
+        <div className="wrapper-color black">
+            <div className="wrapper-color--container">
+                <Back />
+                <div className="wrapper-color--container-master">
+                    <button className="wrapper-color--container-master-button">choose your path again, Padawan</button>
+                    <div className="wrapper-color--container-master-image">
+                        <img src={DarthVader} />
+                    </div>
+                    <div className="wrapper-color--container-master-name">
+                        <h3>Your master is <span>Darth Vader</span></h3>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
