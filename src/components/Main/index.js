0d1dbe1cfaef34fd77e75ae9d823d84b840618be
Paulo Costa <paulofrontend@gmail.com>
2021-08-25 23:53:34 -0300

    Adjust in geral to receive request

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,15 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { A } from 'hookrouter'
 
 import './main.scss'
 
 const Main = () => {
+    const getLight = 'https://swapi.dev/api/people/1'
+    const getDark = 'https://swapi.dev/api/people/4'
+
+    const [master, setMaster] = useState('')
+
+    const handleSortMaster = async () => {
+        try {
+
+            const response = await Promise.race([
+                fetch(getLight),
+                fetch(getDark),
+            ]);
+
+            const result = await response.json();
+
+
+            if (result.name === "Luke Skywalker") {
+                setMaster(result.name.replace(/\s+/g, ""))
+            } else if (result.name === "Darth Vader") {
+                setMaster(result.name.replace(/\s+/g, ""))
+            }
+
+
+        } catch (error) {
+            console.log('ERROR')
+        }
+    }
+
+    useEffect(() => {
+        handleSortMaster()
+    }, [])
+
+
     return (
         <div className="wrapper-main">
             <h1 className="wrapper-main--title">Welcome to <span>iClinic</span></h1>
             <h2 className="wrapper-main--subtitle">frontend challenge</h2>
-            <A href="/master" className="wrapper-main--link">start</A>
-        </div>
+            <A href={`/master/${master}`} onClick={() => handleSortMaster} className="wrapper-main--link">start</A>
+        </div >
     )
 }
 
--- src/components/Masters/index.js
+++ src/components/Masters/index.js
@@ -1,25 +1,25 @@
 import React from 'react'
 import Back from '../GoBack'
+import Image from '../Characters'
+import Name from '../MasterName'
 
 import './master.scss'
-import DarthVader from '../../assets/images/darth-vader.png'
 
-const Master = () => {
+const Master = ({ param }) => {
+
+    console.log(param, 'aeaea')
+
     return (
-        <div className="wrapper-color black">
+        <div className={`wrapper-color ${param === 'LukeSkywalker' ? 'yellow' : 'black'}`}>
             <div className="wrapper-color--container">
                 <Back />
                 <div className="wrapper-color--container-master">
                     <button className="wrapper-color--container-master-button">choose your path again, Padawan</button>
-                    <div className="wrapper-color--container-master-image">
-                        <img src={DarthVader} />
-                    </div>
-                    <div className="wrapper-color--container-master-name">
-                        <h3>Your master is <span>Darth Vader</span></h3>
-                    </div>
+                    <Image character={param} />
+                    <Name master={param} />
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
