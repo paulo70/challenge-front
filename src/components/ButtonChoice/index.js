d64f03eec538565072646ad1714a9367dd583050
Paulo Costa <paulofrontend@gmail.com>
2021-08-26 17:13:20 -0300

    Adjust css order

--- src/components/ButtonChoice/index.js
+++ src/components/ButtonChoice/index.js
@@ -4,9 +4,7 @@ import './button.scss'
 
 const Button = ({ person, handlefn, value }) => {
     return (
-        <div className='wrapper-button'>
-            <button disabled={value} className={person !== 'LukeSkywalker' ? 'bgWhite' : 'bgBlack'} onClick={handlefn}>choose your path again, Padawan</button>
-        </div >
+        <button disabled={value} className={person !== 'LukeSkywalker' ? 'bgWhite' : 'bgBlack'} onClick={handlefn}>choose your path again, Padawan</button>
     )
 }
 
