aac79e7c9b00bfab21d6bad15405d44e4b55db83
Paulo Costa <paulofrontend@gmail.com>
2021-08-26 15:18:40 -0300

    Addes action disabled to button when do an request

--- src/components/ButtonChoice/index.js
+++ src/components/ButtonChoice/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 import './button.scss'
 
-const Button = ({ person }) => {
+const Button = ({ person, handlefn, value }) => {
     return (
         <div className="wrapper-button">
-            <button className={person !== 'LukeSkywalker' ? 'bgWhite' : 'bgBlack'}>choose your path again, Padawan</button>
+            <button disabled={value} className={person !== 'LukeSkywalker' ? 'bgWhite' : 'bgBlack'} onClick={handlefn}>choose your path again, Padawan</button>
         </div >
     )
 }
--- src/components/Masters/index.js
+++ src/components/Masters/index.js
@@ -1,23 +1,70 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Back from '../GoBack'
 import Image from '../Characters'
 import Name from '../MasterName'
 import Button from '../ButtonChoice'
+import Loading from '../Loading'
 
 import './master.scss'
 
 const Master = ({ param }) => {
+    const getLight = 'https://swapi.dev/api/people/1'
+    const getDark = 'https://swapi.dev/api/people/4'
+
+    const [URL, setURL] = useState(param)
+    const [loading, setLoading] = useState(false)
+    const [disabled, setDisabled] = useState(false)
 
     console.log(param, 'aeaea')
+    console.log('ulr', URL)
+
+
+
+    const handleSortMasterURL = async () => {
+        try {
+
+            setLoading(true)
+            setDisabled(true)
+
+            const response = await Promise.race([
+                fetch(getLight),
+                fetch(getDark),
+            ]);
+
+            const outcome = await response.json();
+
+
+            if (outcome.name === "Luke Skywalker") {
+                setURL(outcome.name.replace(/\s+/g, ""))
+            } else if (outcome.name === "Darth Vader") {
+                setURL(outcome.name.replace(/\s+/g, ""))
+            }
+
+            setLoading(false)
+            setDisabled(false)
+
+
+        } catch (error) {
+            console.log('ERROR')
+        }
+    }
+
+    useEffect(() => {
+        console.log(param, 'teste')
+
+    }, [URL])
 
     return (
-        <div className={`wrapper-color ${param === 'LukeSkywalker' ? 'yellow' : 'black'}`}>
+        <div className={`wrapper-color ${URL === 'LukeSkywalker' ? 'yellow' : 'black'}`}>
+            {loading && (
+                <Loading />
+            )}
             <div className="wrapper-color--container">
-                <Back color={param} />
+                <Back color={URL} />
                 <div className="wrapper-color--container-master">
-                    <Button person={param} />
-                    <Image character={param} />
-                    <Name master={param} />
+                    <Button person={URL} handlefn={() => handleSortMasterURL()} value={disabled} />
+                    <Image character={URL} />
+                    <Name master={URL} />
                 </div>
             </div>
         </div >
