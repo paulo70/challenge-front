0aafcd9cf2824129bd1601eb65e7c4bf747753fc
Paulo Costa <paulofrontend@gmail.com>
2021-08-26 15:19:11 -0300

    Added loading component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './loading.scss'
+
+const Loading = () => {
+    return (
+        <div className="wrapper-loading">
+            <div className="wrapper-loading--spiner"></div>
+        </div>
+    )
+}
+
+export default Loading
